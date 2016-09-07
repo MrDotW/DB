109dad4bd93d09492b3349aca43b7fe569a82b3a
weiiihaooo <mr.w.reg@gmail.com>
2016-09-07 10:33:08 +0800

    to auto load page

--- inject3.js
+++ inject3.js
@@ -6,24 +6,29 @@ window.onload = setTimeout(function () {
 ext.runtime.onMessage.addListener(function (msg) {
   if (msg[0] == "cleanAll") {
     var i = document.querySelectorAll("a.page.S_txt1");
-    i = parseInt(i[i.length - 2].innerText);
-    var p = location.search ? location.search.replace(/.+?(\d+)/, "$1") : 1
-    if (p <= i) {
-      var as = document.querySelectorAll("a[action-type=delComment]");
-      var l = as.length;
-      i = 0;
-      var del = function (a) {
-        a.click();
-        setTimeout(function () { document.querySelector("a.W_btn_a.btn_34px").click() }, 500)
-        setTimeout(function () { (++i < l) && del(as[i]) }, 600)
+    if (i) {
+      i = parseInt(i[i.length - 2].innerText);
+      var p = location.search ? location.search.replace(/.+?(\d+)/, "$1") : 1
+      if (p <= i + 2) {
+        var as = document.querySelectorAll("a[action-type=delComment]");
+        var l = as.length;
+        i = 0;
+        var del = function (a) {
+          a.click();
+          setTimeout(function () { document.querySelector("a.W_btn_a.btn_34px").click() }, 500)
+          setTimeout(function () { (++i < l) && del(as[i]) }, 600)
+        }
+        if (l) {
+          del(as[i]);
+          setTimeout(function () { location.replace(`http://weibo.com/comment/outbox?page=${p}`) }, 1100 * l + 2000)
+        } else {
+          p++;
+          location.replace(`http://weibo.com/comment/outbox?page=${p}`)
+        }
       }
-      if (l) {
-        del(as[i]);
-        setTimeout(function () { location.replace(`http://weibo.com/comment/outbox?page=${p}`) }, 1100 * l + 2000)
-      } else {
-        p++;
-        location.replace(`http://weibo.com/comment/outbox?page=${p}`)
-      }
+    } else {
+      i = document.querySelector("i.W_icon.icon_rederrorS");
+      i && i.nextElementSibling.click()
     }
   }
 })
